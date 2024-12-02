86c75c3754c63b0437f5db84064b4b5d4add360d
Mikhail Stepanov <mi.stepanov@innopolis.university>
2024-12-03 02:57:49 +0300

    feature: add email sending

--- code/utils/email_utils.py
+++ code/utils/email_utils.py
@@ -1,10 +1,24 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from config.settings import EMAIL_ADDRESS, EMAIL_PASSWORD, EMAIL_RECIPIENTS
 
 def send_email(subject, body):
     try:
-        for recipient in EMAIL_RECIPIENTS:
-            print(f"Email sent to {recipient} from {EMAIL_ADDRESS}:\n" +
-                  f"Subject: {subject}\n" +
-                  f"Body: {body}")
+        with smtplib.SMTP('smtp.yandex.ru', 587) as server:
+            server.starttls()  
+            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+            for recipient in EMAIL_RECIPIENTS:
+                msg = MIMEMultipart()
+                msg['From'] = EMAIL_ADDRESS
+                msg['To'] = recipient
+                msg['Subject'] = subject
+                msg.attach(MIMEText(body, 'plain'))
+
+                server.send_message(msg)
+                # print(f"Email sent to {recipient} from {EMAIL_ADDRESS}:\n" +
+                #       f"Subject: {subject}\n" +
+                #       f"Body: {body}")
     except Exception as e:
         print(f"Error sending email: {e}")
